698b48256c43a93e135e796ea39dddc68a57c368
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2022-04-16 13:39:34 +0200

    Added immidiate death in the ocean

---
+++ Polyaris/Assets/Scripts/Other/OceanDeath.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.UltimateCharacterController.Traits;
+
+
+public class OceanDeath : MonoBehaviour
+{
+    [Tooltip("The character that has the health component.")]
+    [SerializeField] protected GameObject m_Character;
+    /// <summary>
+    /// Damages and heals the character.
+    /// </summary>
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var health = m_Character.GetComponent<Health>();
+
+            health.ImmediateDeath();
+        }
+    }
+}
