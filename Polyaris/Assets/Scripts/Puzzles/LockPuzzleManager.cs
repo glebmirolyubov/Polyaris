4005f327a73247204d8ec4bd9c8daaa1b8c8abc2
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2022-05-29 22:04:38 +0200

    Added padlock puzzle

--- Polyaris/Assets/Scripts/Puzzles/LockPuzzleManager.cs
+++ Polyaris/Assets/Scripts/Puzzles/LockPuzzleManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class LockPuzzleManager : MonoBehaviour
 {
     public GameObject openDoorInteraction;
-    public GameObject puzzleUI;
 
     bool puzzleSolved = false;
 
@@ -17,31 +16,7 @@ public class LockPuzzleManager : MonoBehaviour
     public void SolvePuzzle()
     {
         openDoorInteraction.SetActive(true);
-        puzzleUI.SetActive(false);
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
         puzzleSolved = true;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && !puzzleSolved)
-        {
-            Debug.Log("Player Detected!");
-            puzzleUI.SetActive(true);
-            //Set Cursor to be visible
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player") && !puzzleSolved)
-        {
-            puzzleUI.SetActive(false);
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-    }
 }
